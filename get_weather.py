7cecb2efca97546bb5cea75832905c1dfdaeb592
Ilia Ilmer <i.ilmer@icloud.com>
2019-11-18 19:59:37 -0500

    added requirements.txt

--- get_weather.py
+++ get_weather.py
@@ -1,12 +1,12 @@
 import pyowm
-from numpy import random
+import numpy as np
 from pyowm.tiles.enums import MapLayerEnum
 from pyowm.commons.tile import Tile
 from pyowm.utils.geo import Point
 
 
 def get_observation():
-    lat, lon = random.uniform(-90., 90.), random.uniform(-180., 180.)
+    lat, lon = np.random.uniform(-90., 90.), np.random.uniform(-180., 180.)
 
     owm = pyowm.OWM('03a2f49a1242c4094d3593b314fff7f0')
 
